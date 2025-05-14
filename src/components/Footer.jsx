a25e1ac0e548dd5f7985709622ba1dbe1b89a7f9
Diana Le <le.diana.d@gmail.com>
2025-05-13 22:24:40 -0500

    Add footer and styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/pursuitofleisure/tenzies"
+        className="footer-link"
+        target="_blank"
+      >
+        View on Github
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
